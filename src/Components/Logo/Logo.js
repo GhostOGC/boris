f56504a1df73615638d30ba77faf859420deb7a9
Victor Nachkov <victornachkov@gmail.com>
2021-11-22 17:39:16 +0200

    feat: Header- complete header

---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,6 @@
+import logo from "./Logo.png";
+
+const Logo = () => {
+  return <img className="main-logo" alt="Main Logo" src={logo} />;
+};
+export default Logo;
--- src/Components/Static/Header.js
+++ src/Components/Static/Header.js
@@ -1,11 +1,29 @@
 import { Link } from "react-router-dom";
+import Logo from "../Logo/Logo";
 
 const Header = () => {
   return (
-    <div>
-      <Link to="/">Projects</Link>
-      <Link to="/bio">Bio</Link>
-      <Link to="/contacts">Contacts</Link>
+    <div className="header">
+      <Link className="header-logo-link" to="/">
+        <Logo />
+      </Link>
+
+      <div className="main-nav-wrapper">
+        <div className="social-wrapper">
+          <a className="social-item " href="https://vimeo.com/kalaidjiev">
+            <i className="fab fa-vimeo-square grey-hover" />
+          </a>
+          <a
+            className="social-item "
+            href="https://www.instagram.com/b.kalaidjiev/"
+          >
+            <i className="fab fa-instagram-square grey-hover" />
+          </a>
+        </div>
+        <span>
+          <i onClick={() => console.log("clicked")} className="fas fa-bars" />
+        </span>
+      </div>
     </div>
   );
 };
